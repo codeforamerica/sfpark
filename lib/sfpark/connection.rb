287e90521c4606f15133703c227a20a565058ccc
Ryan Resella <ryan@codeforamerica.org>
2011-10-27 14:56:01 -0700

    Rashify since all the fields are in upper case

--- lib/sfpark/connection.rb
+++ lib/sfpark/connection.rb
@@ -8,6 +8,7 @@ module SfPark
       Faraday.new(:url => 'http://api.sfpark.org/sfpark/rest') do |connection|
         connection.use Faraday::Request::UrlEncoded
         connection.use Faraday::Response::RaiseError
+        connection.use Faraday::Response::Rashify
         connection.use Faraday::Response::Mashify
         connection.use Faraday::Response::ParseJson
         connection.adapter(Faraday.default_adapter)
--- sfpark.gemspec
+++ sfpark.gemspec
@@ -7,6 +7,7 @@ Gem::Specification.new do |gem|
   gem.add_dependency 'hashie', '~> 1.1.0'
   gem.add_dependency 'multi_json', '~> 1.0.2'
   gem.add_dependency 'multi_xml', '~> 0.4.0'
+  gem.add_dependency 'rash', '~> 0.3.0'
   gem.add_development_dependency 'rake', '~> 0.9'
   gem.add_development_dependency 'rdiscount', '~> 1.6'
   gem.add_development_dependency 'rspec', '~> 2.6'

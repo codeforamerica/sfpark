13fb2d3240fe56b52e47931673f0ed40b7f76a6b
Ryan Resella <ryan@codeforamerica.org>
2011-10-27 18:11:59 -0700

    Added a spec for passing params

--- spec/sfpark/availability_spec.rb
+++ spec/sfpark/availability_spec.rb
@@ -17,5 +17,18 @@ describe SfPark::Client::Availability do
         search.num_records.should == "25"
       end
     end
+
+    context "with query paramaters" do
+      before do
+        stub_get("availabilityservice?lat=37.792275&long=-122.397089&radius=0.25&uom=mile&response=json").
+          to_return(:status => 200, :body => fixture("availability_params.json"))
+      end
+
+      it "should return the num of records available" do
+        search = @client.availability(:lat => 37.792275,:long => -122.397089,:radius => 0.25, :uom => 'mile')
+        search.num_records.should == "67"
+      end
+    end
+
   end
 end
